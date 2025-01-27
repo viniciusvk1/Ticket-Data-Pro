44a32e4bd3219ef8891a37530bdeb5784d639124
Vinicius Almeida <vinicius.almeida@gruposc.com.br>
2025-01-27 17:26:27 -0300

    adding queries

--- src/main/java/com/github/viniciusvk1/Ticket/Data/Pro/repository/TicketRepository.java
+++ src/main/java/com/github/viniciusvk1/Ticket/Data/Pro/repository/TicketRepository.java
@@ -2,6 +2,26 @@ package com.github.viniciusvk1.Ticket.Data.Pro.repository;
 
 import com.github.viniciusvk1.Ticket.Data.Pro.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Query("SELECT t FROM Ticket t WHERE t.status = 'Abertos'")
+    List<Ticket> findOpenTickets();
+
+    @Query("SELECT t FROM Ticket t WHERE t.nomeDesenvolvedor = :desenvolvedor")
+    List<Ticket> findTicketsByDeveloper(@Param("desenvolvedor") String desenvolvedor);
+
+    @Query("SELECT t FROM Ticket t WHERE t.statusDocumento = 'Reprovado'")
+    List<Ticket> findRejectedTickets();
+
+    @Query("SELECT t FROM Ticket t WHERE t.status = 'Em Progresso'")
+    List<Ticket> findActiveTickets();
+
+    @Query("SELECT t FROM Ticket t WHERE t.prioridade = :prioridade")
+    List<Ticket> findTicketsByPriority(@Param("prioridade") String prioridade);
+
 }

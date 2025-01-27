9e7f26fab79c6255bc44cc69c5e53367e1a966c7
Vinicius Almeida <vinicius.almeida@gruposc.com.br>
2025-01-27 13:13:46 -0300

    initial files

---
+++ src/main/java/com/github/viniciusvk1/TicketDataPro/TicketDataProApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.TicketDataPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDataProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketDataProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viniciusvk1/TicketDataPro/TicketDataProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.TicketDataPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDataProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
